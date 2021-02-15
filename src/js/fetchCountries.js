ffa7297c151872bb951a07b521a21c3841c73b67
Katerina Zamiatina <eozamyatina@gmail.com>
2021-02-15 10:23:13 +0200

    fixes

--- src/js/fetchCountries.js
+++ src/js/fetchCountries.js
@@ -2,9 +2,7 @@ function fetchCountries(country) {
   const url = `https://restcountries.eu/rest/v2/name/${country}`;
   return fetch(url)
     .then(res => res.json())
-    // .then({ name })
     .catch(error => console.log(error));
-  // .catch(error({ text: 'Something went wrong', delay: 1000 }));
 }
 
 export default fetchCountries;
